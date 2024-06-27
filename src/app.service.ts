747eac02287fe5d13afc288019f5d517b26a4e6a
Ubuntu <ubuntu@ip-172-31-59-29.ec2.internal>
2024-06-27 11:09:13 +0000

    new service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Guru!!!';
+    return 'Hello World from Buzo!!!';
   }
 
   getPing(): string {
